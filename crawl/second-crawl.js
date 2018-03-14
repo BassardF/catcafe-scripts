4a176570292570564466acce0e59d4e04b5498fb
Frank Bassard <f.bassard@gmail.com>
2018-03-15 11:21:05 +1300

    more upload

--- crawl/second-crawl.js
+++ crawl/second-crawl.js
@@ -6,44 +6,55 @@ const queries = require('./queries.json');
 const GOOGLE_PLACES_API_KEY = "AIzaSyD-p7FodgtCx3iZMKWf0_FjJPAjG6kJo5k";
 const GOOGLE_PLACES_OUTPUT_FORMAT = "json";
 
-// const normalizedPath = require("path").join(__dirname, "./json");
-//
-// fs.readdirSync(normalizedPath).forEach(function(file) {
-//   const data = require("./json/" + file);
-//   checkValidity(file, data);
-// });
-const path = 'cafe_chat_lyon.json';
-const data = require('./json/'+path);
-let results = [], count = 0, target = 0;
+const banlist = require('./place-id-banlist.json');
 
-if(data.results){
-  target = data.results.length;
-  for (var i = 0; i < data.results.length; i++) {
-    fetchDetails(GOOGLE_PLACES_OUTPUT_FORMAT, GOOGLE_PLACES_API_KEY, data.results[i]);
+const normalizedPath = require("path").join(__dirname, "./json");
+
+var dc = 0, counts = {};
+fs.readdirSync(normalizedPath).forEach(function(file) {
+  const data = require("./json/" + file);
+  secondCrawl(file, data);
+});
+// const path = 'cafe_chat_lyon.json';
+// const data = require('./json/'+path);
+
+function secondCrawl(file, data){
+  let results = [], target = 0;
+
+  if(data.results){
+    target = data.results.length;
+    counts[file] = 0;
+    for (var i = 0; i < data.results.length; i++) {
+      if (data.results[i].place_id && !banlist.includes(data.results[i].place_id)) {
+        fetchDetails(GOOGLE_PLACES_OUTPUT_FORMAT, GOOGLE_PLACES_API_KEY, data.results[i], results, file, target);
+      }
+    }
   }
 }
 
-function fetchDetails(format, key, place){
-  fetch(`https://maps.googleapis.com/maps/api/place/details/${format}?key=${key}&placeid=${place.place_id}`).then(
-    (response) => {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' + response.status);
-        return;
-      }
-      // Examine the text in the response
-      response.json().then(function(data) {
-        place.place_details = data;
-        results.push(place);
-        count++;
-        if(count == target) output();
-        else console.log(count , target);
-      });
+function fetchDetails(format, key, place, results, file, target){
+  console.log(`https://maps.googleapis.com/maps/api/place/details/${format}?key=${key}&placeid=${place.place_id}`);
+  fetch(`https://maps.googleapis.com/maps/api/place/details/${format}?key=${key}&placeid=${place.place_id}`)
+  .then((response) => {
+    if (response.status !== 200) {
+      console.log('Looks like there was a problem. Status Code: ' + response.status);
+      return;
     }
-  ).catch(function(err) {
+    // Examine the text in the response
+    response.json().then(function(data) {
+      place.place_details = data;
+      results.push(place);
+      counts[file]++;
+      if(counts[file] == target) output(results, file);
+      else console.log(counts[file] , target);
+    }).catch(function(err) {
+      console.log('Sec Fetch Error :-S', err);
+    });
+  }).catch(function(err) {
     console.log('Fetch Error :-S', err);
   });
 }
 
-function output(){
-  fs.writeFile('second-json/' + path, JSON.stringify(results), 'utf8');
+function output(results, file){
+  fs.writeFile('second-json/' + file, JSON.stringify(results), 'utf8');
 }
--- crawl/third-upload.js
+++ crawl/third-upload.js
@@ -5,6 +5,23 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
+const cityHelper = {
+  "catcafe_bangkok.json" : "bangkok",
+  "catcafe_buenos_aires.json" : "buenos-aires",
+  "catcafe_chiang_mai.json" : "chiang-mai",
+  "catcafe_dublin.json" : "dublin",
+  "catcafe_edinburgh.json" : "edinburgh",
+  "catcafe_ho_chi_minh.json" : "ho-chi-minh",
+  "catcafe_hongkong.json" : "hong-kong",
+  "catcafe_istanbul.json" : "istanbul",
+  "catcafe_manchester.json" : "manchester",
+  "catcafe_new_york.json" : "new_york",
+  "catcafe_prague.json" : "prague",
+  "catcafe_rio_de_janeiro.json" : "rio-de-janeiro",
+  "catcafe_saigon.json" : "saigon",
+  "catcafe_shangai.json" : "shangai"
+};
+
 const db = admin.firestore();
 
 const normalizedPath = require("path").join(__dirname, "./second-json");
@@ -12,26 +29,45 @@ const fs = require("fs");
 
 fs.readdirSync(normalizedPath).forEach(function(file) {
   const data = require("./second-json/" + file);
-  upload(file, data);
+  let uploaded = null, uploadedArray = null;
+  try {
+    uploaded = require("./third-json/" + file);
+    uploadedArray = uploaded.map((x) => x.place_id);
+  } catch(e){}
+  upload(file, data, uploaded, uploadedArray);
 });
 
-function upload(file, data){
-  console.log("## Uploading data from : ", file);
+function upload(file, data, banned, bannedArray){
+  console.log("## Uploading data from : ", file, !!banned);
   let uploaded = [];
   for (var i = 0; i < data.length; i++) {
-    let {country, city, toUpload} = formatData(data[i]);
-
-    let ref = db.collection('countries')
-                .doc(country)
-                .collection('cities')
-                .doc(city)
-                .collection('catcafes');
-    let docRef = ref.doc();
-    console.log("- ", country, city, docRef.id);
-    uploaded.push({country, city, place_id: toUpload.place_id})
-    docRef.set(toUpload);
+    if(!bannedArray || (data[i].place_id && !bannedArray.includes(data[i].place_id))) {
+      let {country, city, toUpload} = formatData(data[i]);
+      if(!city && cityHelper[file]) city = cityHelper[file];
+      if(country && city && toUpload){
+        let ref = db.collection('countries')
+                    .doc(country)
+                    .collection('cities')
+                    .doc(city)
+                    .collection('catcafes');
+        let docRef = ref.doc();
+        console.log("- ", country, city, docRef.id);
+        uploaded.push({country, city, place_id: toUpload.place_id})
+        docRef.set(toUpload);
+      } else {
+        console.log("!!!! ", country, city, toUpload);
+      }
+    } else if(data[i].place_id){
+      console.log("* already uploaded : ", data[i].place_id);
+      let index = bannedArray.indexOf(data[i].place_id);
+      uploaded.push({
+        country: banned[index].country,
+        city: banned[index].city,
+        place_id: banned[index].place_id
+      });
+    }
   }
-  fs.writeFile('third-json/' + file, JSON.stringify(uploaded), 'utf8');
+  fs.writeFile('third-json/' + file, JSON.stringify(uploaded), () => {});
 }
 
 
@@ -47,7 +83,7 @@ function formatData(data){
     opening_hours: data.place_details.result.opening_hours,
     website: data.place_details.result.website
   };
-  delete toUpload.opening_hours.open_now;
+  if(toUpload.opening_hours && toUpload.opening_hours.open_now !== undefined) delete toUpload.opening_hours.open_now;
 
   let country = "", city = "";
   for (var i = 0; i < data.place_details.result.address_components.length; i++) {
@@ -56,5 +92,9 @@ function formatData(data){
     if(comp.types && comp.types.includes("locality")) city = comp.long_name.toLowerCase().split(" ").join("-");
   }
 
-  return {toUpload, country, city};
+  return {
+    toUpload: JSON.parse(JSON.stringify(toUpload)),
+    country,
+    city
+  };
 }
